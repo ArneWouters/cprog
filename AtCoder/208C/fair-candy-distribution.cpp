7053dd62b23e02e2ec02503a42ab0e559d223252
Arne Wouters <arne@wouters.co>
2021-07-07 16:08:50 +0200

    Add solution AtCoder 208C

---
+++ AtCoder/208C/fair-candy-distribution.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define nimble ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define Fill(a,b)  memset(a,b,sizeof(a))
+
+// DEBUG FUNCTIONS START
+void __print(int x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V> void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void deb() {cerr << "\n";}
+template <typename T, typename... V> void deb(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; deb(v...);}
+// DEBUG FUNCTIONS END
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const ll MOD = 1e9 + 7;
+
+void solve() {
+  ll n,k; cin >> n >> k;
+  vector<int> citizens(n);
+  for (auto &x: citizens) cin >> x;
+  ll p = k / n;
+  ll r = k % n;
+  vector<int> sorted_citizens(citizens);
+  sort(sorted_citizens.begin(), sorted_citizens.end());
+  ll cutoff = sorted_citizens[r];
+
+  for (const auto &x: citizens) {
+    if (x < cutoff) cout << p+1 << endl;
+    else cout << p << endl;
+  }
+}
+
+int main() {
+  nimble;
+  // freopen("input", "r", stdin);
+  // freopen("output", "w", stdout);
+
+  int tc; tc = 1;
+  while (tc--) {
+    // cout << "Case #" << t << ": " << endl;
+    solve();
+  }
+
+  return 0;
+}
