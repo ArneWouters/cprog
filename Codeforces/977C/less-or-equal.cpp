fc793c753e02701b7b387fbded6569737c6ea9dd
Arne Wouters <arne@wouters.co>
2021-07-09 22:09:43 +0200

    Add solutions of Codeforces 977A, B & C

---
+++ Codeforces/977C/less-or-equal.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define nimble ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define Fill(a,b)  memset(a,b,sizeof(a))
+
+// DEBUG FUNCTIONS START
+void __print(int x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V> void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void deb() {cerr << "\n";}
+template <typename T, typename... V> void deb(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; deb(v...);}
+// DEBUG FUNCTIONS END
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const ll MOD = 1e9 + 7;
+
+void solve() {
+ int n,k; cin >> n >> k;
+ vector<int> arr(n);
+ for (auto &x: arr) cin >> x;
+
+ sort(arr.begin(), arr.end());
+
+ if (n == 0) cout << -1 << endl;
+ else if (k == 0) cout << (arr[0] == 1 ? -1 : 1) << endl;
+ else if (k == n) cout << arr[n-1] << endl;
+ else if (arr[k-1] == arr[k]) cout << -1 << endl;
+ else cout << arr[k-1] << endl;
+
+}
+
+int main() {
+  nimble;
+  // freopen("input", "r", stdin);
+  // freopen("output", "w", stdout);
+
+  int tc; tc = 1;
+  while (tc--) {
+    // cout << "Case #" << t << ": " << endl;
+    solve();
+  }
+
+  return 0;
+}
