a9050de0249b54bb3d9c91e811c8762fa686db59
Arne Wouters <arne@wouters.co>
2021-07-23 16:12:39 +0200

    Add solution of Codeforces 1550A

---
+++ Codeforces/1550A/find-the-array.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define nimble ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define Fill(a,b)  memset(a,b,sizeof(a))
+
+// DEBUG FUNCTIONS START
+void __print(int x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V> void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void deb() {cerr << "\n";}
+template <typename T, typename... V> void deb(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; deb(v...);}
+// DEBUG FUNCTIONS END
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const ll MOD = 1e9 + 7;
+
+void solve() {
+  int s; cin >> s;
+  int ans = 1;
+  int sum = 1;
+
+  while (sum < s) {
+    if (s - sum < 2*ans + 1) {
+      ans++;
+      cout << ans << endl;
+      return;
+    }
+
+    sum += (2*ans + 1);
+    ans++;
+  }
+
+  cout << ans << endl;
+
+}
+
+int main() {
+  nimble;
+  // freopen("input", "r", stdin);
+  // freopen("output", "w", stdout);
+
+  int tc; cin >> tc;
+  while (tc--) {
+    // cout << "Case #" << t << ": " << endl;
+    solve();
+  }
+
+  return 0;
+}
